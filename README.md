722440161335872ead3d4d501764d83e7959814b
theo <krueger.theo@gmail.com>
2021-06-25 15:46:50 -0400

    add storage and cleanup some garbage

--- README.md
+++ README.md
@@ -1,39 +1,56 @@
 # dwm-bar
-personalised fork of [dwm-bar](https://github.com/joestandring/dwm-bar), a modular statusbar for dwm
+complete rewrite (for the worse) of [dwm-bar](https://github.com/joestandring/dwm-bar), a modular statusbar for dwm
 ## directory
-- [current functions](#current-functions)
+- [current-functions](#current-functions)
   - [dwm_track](#dwm_track)
   - [dwm_memory](#dwm_memory)
+  - [dwm_storage](#dwm_storage)
   - [dwm_date](#dwm_date)
 - [installation](#installation)
 - [usage](#usage)
 - [customisation](#customisation)
+- [contribution](#contribution)
 - [acknowledgements](#acknowledgements)
-
 ## current-functions
 ### dwm_track
 displays what is now playing using playerctl metadata
+
 format:
 ```
 <status>: <track title> <positon>/<length>
 ```
 display:
 ```
-Playing: Touhiron 3:42/4:35
+Playing: Touhiron 03:42/04:35
 ```
 dependencies: `playerctl`
 ### dwm_memory
 displays used memory
+
 format:
 ```
-mem: <usedmem>Gi
+mem: <usedmem><unit of storage>
 ```
 display:
 ```
 mem: 6.9Gi
 ```
+### dwm_storage
+cycles printing all storage drives and their % usage
+
+this module tries to keep using the same amount of space on the bar so it does not distrub other modules
+
+format:
+```
+<spacing><disk> <percent used>
+```
+display:
+```
+sda 42%
+```
 ### dwm_date
 displays current date and time
+
 format:
 ```
 [<weekday>] <fullyear>-<month>-<day> <time>
@@ -64,7 +81,13 @@ $ ./dwm_bar.sh
 if you would like your bar to be displayed when x starts, add it to your `.xinitrc` before launching dwm
 ## customisation
 my personalisation of dwm_bar includes stripping it of its modularity in favour of simplicity. creating many modules may cause headache, but i prefer it this way.
+
 you define functions in `dwm_bar.sh` and add them to the `xsetroot` loop with manual spacing and order.
+## contribution
+contributing to this is a simple 3-step process:
+* use echo instead of printf wherever possible
+* no capitalisation
+* constantly question why you made this when dwm-bar is outright better and easier to modify
 ## acknowledgements
 * [Klemens Nanni](https://notabug.org/kl3)
 * [@boylemic](https://github.com/boylemic/configs/blob/master/dwm_status)
--- dwm_bar.sh
+++ dwm_bar.sh
@@ -1,50 +1,82 @@
 #!/bin/sh
 
-# heavily personalised fork of [dwm-bar](https://github.com/joestandring/dwm-bar), a modular statusbar for dwm
+# complete rewrite of [dwm-bar](https://github.com/joestandring/dwm-bar), a modular statusbar for dwm
 # Theo Krueger <git@github.com/theokrueger>
 # GNU GPLv3
 # dependencies: xorg-xsetroot playerctl
 
-# define our functions
+# add a loop number variable so we can loop through some things nicely
+dwm_bar_loops=0
 
 #dwm_track: a dwm_bar function to display now playing track
-dwm_track () { # depends on playerctl
+dwm_track() { # depends on playerctl
     # prints now playing track in format:
     # <status>: <track title> <positon>/<length>
     # for example:
-    # Playing: Touhiron 3:42/4:35
+    # Playing: Touhiron 03:42/04:35
     # IMPORTANT NOTE, since this can return nothing we add the separation between modules in this function instead of the concatenation during xsetroot
     status=$(playerctl status -s)
     if [ $status = "Playing" ]; then
         pos=$(playerctl position -s | sed 's/..\{6\}$//')
         len=$(playerctl metadata mpris:length -s | sed 's/.\{6\}$//')
+        # dumb print setup because i do not care
         printf "$status: $(playerctl metadata title -s) %02d:%02d/%02d:%02d | " $((pos / 60)) $((pos % 60)) $((len / 60)) $((len % 60))
     fi
 }
 
 # dwm_memory: a dwm_bar function to display used memory
-dwm_memory () {
+dwm_memory() {
     # prints memory usage in format:
-    # mem: <usedmem>Gi
+    # mem: <usedmem><unit of storage>
     # for example:
     # mem: 6.9Gi
-	printf "mem: %s" "$(free -h | grep Mem | awk '{print $3}')"
+    printf "mem: %s" "$(free -h | grep Mem | awk '{print $3}')"
+}
+
+# dwm_storage: a dwm_bar function to display used storage
+# IMPORTANT: cycles through all storages mounted before execution of this script ONLY
+dwm_storage_drives="$(df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6-)"
+dwm_storage_len="$(( $(df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6- | awk '{print length, $0}' | sort -nr | head -n 1 | cut -c1-1) + 5 ))" # TODO please fix thanks
+dwm_storage_nod="$(echo $dwm_storage_drives | wc -w)"
+dwm_storage_drives=($dwm_storage_drives)
+# here is the entire process so i can actually understand what i wrote:
+    # upon launching script, get a list of all drives, taking only the names with a leading /dev/, then substringing out the /dev/
+    # save the length of the longest name to a variable
+    # then we save the total number of drives to another variable so we dont have to do that calculation again
+    # and finally to finish the setup we assign all drives to an array for later use
+    # when dwm_storage is called, we extract the drive we want to print the info of based off which cycle we are on.
+        # this is done by using the array of all drives, getting what drive we should be at by modding cycles by 3*nod then dividing out 3
+        # this makes the same drive stay for 3 cycles
+    # then we add the % storage use with df <drive>, taking out the % to make <10% use still 2 digits long and readding it
+    # we then calculate the number of spaces so that we do not disturb the positions of everything else on the bar every 3 cycles.
+        # take the longest name and add 4 to it to get the longest possible length (<name><space><xx%> = +4)
+        # take the maximum length minus current length of storage and printf that many spaces
+    # finally we print in format "<spacing><disk> <percent used>"
+dwm_storage() {
+    # cycles printing all storage drives and their % usage in format:
+    # <spacing><disk> <percent used>
+    # for example:
+    # sda 42%
+    temp="$(echo ${dwm_storage_drives[$(( $dwm_bar_loops % ($dwm_storage_nod * 3) / 3 ))]})"
+    temp="$(echo $temp) $(printf "%02d" "$(df -h /dev/$(echo $temp) | tail -1 | awk '{print $5}' | sed 's/.$//' )")%"
+    echo "sto:$(printf "%*s" $(($dwm_storage_len - ${#temp})))$temp"
 }
 
 # dwm_date: a dwm_bar function to display current time
-dwm_date () { 
+dwm_date() {
     # prints date in format:
     # [<weekday>] <fullyear>-<month>-<day> <time>
     # for example:
     # [4] 1970-01-01 00:00
-    printf "$(date "+[%u] %F %R")"
+    echo "$(date "+[%u] %F %R")"
 }
 
 # update loop
-while true
-do
-    # set name to our new info and do it again 1 second later
-    xsetroot -name "$(dwm_track)$(dwm_memory) | $(dwm_date)"
+while true; do
+    # set name to our new info and do it again 1 second(s) later
     # IMPORTANT NOTE, since dwm_track can return nothing we add the separation in its function, if you change order of things please modify dwm_track accordingly
+    xsetroot -name "$(dwm_track)$(dwm_memory) | $(dwm_storage) | $(dwm_date)"
+    # oincrement our number of loops
+    dwm_bar_loops=$(($dwm_bar_loops + 1))
     sleep 1
 done
