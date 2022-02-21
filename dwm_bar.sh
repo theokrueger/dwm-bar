0896371ca1d35d7ec54344c5cf1b088cdb375b22
theokrueger <theokrueger@airmail.cc>
2022-02-20 22:17:30 -0500

    add dwm_battery() to track battery charge

--- dwm_bar.sh
+++ dwm_bar.sh
@@ -34,11 +34,6 @@ dwm_memory() {
 }
 
 # dwm_storage: a dwm_bar function to display used storage
-# IMPORTANT: cycles through all storages mounted before execution of this script ONLY
-dwm_storage_drives="$(df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6-)"
-dwm_storage_len="$(($(df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6- | awk '{print length, $0}' | sort -nr | head -n 1 | awk '{print $1;}')))"
-dwm_storage_nod="$(echo $dwm_storage_drives | wc -w)"
-dwm_storage_drives=($dwm_storage_drives)
 # here is the entire process so i can actually understand what i wrote:
     # upon launching script, get a list of all drives, taking only the names with a leading /dev/, then substringing out the /dev/
     # save the length of the longest name to a variable
@@ -51,13 +46,29 @@ dwm_storage_drives=($dwm_storage_drives)
         # take the maximum length minus current length of storage and printf that many spaces
     # then we add the % storage use with df <drive>, taking out the % to make <10% use still 2 digits long and readding it
     # finally we print in format "<disk><spacing><percent used>"
+# IMPORTANT: cycles through all storages mounted before execution of this script ONLY
+dwm_storage_drives="$(df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6-)"
+dwm_storage_len="$(($(df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6- | awk '{print length, $0}' | sort -nr | head -n 1 | awk '{print $1;}')))"
+dwm_storage_nod="$(echo $dwm_storage_drives | wc -w)"
+dwm_storage_drives=($dwm_storage_drives)
 dwm_storage() {
     # cycles printing all storage drives and their % usage in format:
     # <disk><spacing><percent used>
     # for example:
     # sda 42%
+    # spacing will always be so that output does not differ in width
     temp="$(echo ${dwm_storage_drives[$(( $dwm_bar_loops % ($dwm_storage_nod * 3) / 3 ))]})"
     echo "$temp$(printf "%*s" $(($dwm_storage_len - ${#temp}))) $(printf "%02d" "$(df -h /dev/$(echo $temp) | tail -1 | awk '{print $5}' | sed 's/.$//' )")%"
+}
+
+# dwm_battery: a dwm_bar funcion to display current battery percentage and state
+dwm_battery() {
+    # prints battery in format:
+    # <status>: <charge>%
+    # for example:
+    # Discharging: 69%
+    # i <3 iso-8601
+    echo "$(cat /sys/class/power_supply/BAT0/status): $(cat /sys/class/power_supply/BAT0/capacity)%"
 }
 
 # dwm_date: a dwm_bar function to display current time
@@ -73,7 +84,7 @@ dwm_date() {
 while true; do
     # set name to our new info and do it again 1 second(s) later
     # IMPORTANT NOTE, since dwm_track can return nothing we add the separation in its function, if you change order of things please modify dwm_track accordingly
-    xsetroot -name "$(dwm_track)$(dwm_memory) | $(dwm_storage) | $(dwm_date)"
+    xsetroot -name "$(dwm_track)$(dwm_memory) | $(dwm_storage) | $(dwm_battery) | $(dwm_date)"
     # oincrement our number of loops
     dwm_bar_loops=$(($dwm_bar_loops + 1))
     sleep 1
