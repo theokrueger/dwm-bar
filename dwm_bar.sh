c4cde9cdefbb66bd2fc7cfabe6989424c9ccff65
theokrueger <theokrueger@airmail.cc>
2022-02-22 18:50:07 -0500

    general styling improvements

--- dwm_bar.sh
+++ dwm_bar.sh
@@ -18,10 +18,10 @@ dwm_track() { # depends on playerctl
     # IMPORTANT NOTE: since this can return nothing we add the separation between modules in this function instead of the concatenation during xsetroot
     status=$(playerctl status -s)
     if [ $status = "Playing" 2> /dev/null ]; then # if playerctl cannot hook into anything then it throws errors so we just throw stderr into the void
-        pos=$(playerctl position -s | sed 's/..\{6\}$//')
-        len=$(playerctl metadata mpris:length -s | sed 's/.\{6\}$//')
+        pos=$( playerctl position -s | sed 's/..\{6\}$//' )
+        len=$( playerctl metadata mpris:length -s | sed 's/.\{6\}$//' )
         # dumb print setup because i do not care
-        printf "$status: $(playerctl metadata title -s) %02d:%02d/%02d:%02d | " $((pos / 60)) $((pos % 60)) $((len / 60)) $((len % 60))
+        printf "$status: $( playerctl metadata title -s ) %02d:%02d/%02d:%02d | " $(( pos / 60 )) $(( pos % 60 )) $(( len / 60 )) $(( len % 60 ))
     fi
 }
 
@@ -33,7 +33,7 @@ dwm_memory() {
     # mem: 6.9Gi
 
     # IMPORTANT NOTE: you can add another argument to 'free' like '--giga' or '--peta' to change scale of the number, read free(1) manpage.
-    printf "mem: %s" "$(free -h | grep Mem | awk '{print $3}')"
+    printf "mem: %s" "$( free -h | grep Mem | awk '{print $3}' )"
 }
 
 # dwm_storage: a dwm_bar function to display used storage
@@ -51,10 +51,10 @@ dwm_memory() {
 #     then we add the % storage use with df <drive>, taking out the % to zer-pad values of <10%. readd the % sign for vanity
 #     finally we print in format "<disk><spacing><percent used>"
 # IMPORTANT: cycles through all storages mounted before execution of this script ONLY
-dwm_storage_drives="$(df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6-)"
-dwm_storage_len="$(($(df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6- | awk '{print length, $0}' | sort -nr | head -n 1 | awk '{print $1;}')))"
-dwm_storage_nod="$(echo $dwm_storage_drives | wc -w)"
-dwm_storage_drives=($dwm_storage_drives)
+dwm_storage_drives="$( df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6- )"
+dwm_storage_len="$(( $( df -h | awk '{ print $1 }' | grep /dev/ | cut -c 6- | awk '{print length, $0}' | sort -nr | head -n 1 | awk '{print $1;}' ) ))"
+dwm_storage_nod="$( echo $dwm_storage_drives | wc -w )"
+dwm_storage_drives=( $dwm_storage_drives )
 dwm_storage() {
     # cycles printing all storage drives and their % usage in format:
     # <disk><spacing><percent used>
@@ -62,8 +62,8 @@ dwm_storage() {
     # sda 42%
 
     # IMPORTANT NOTE: spacing will always be so that output does not differ in width
-    temp="$(echo ${dwm_storage_drives[$(( $dwm_bar_loops % ($dwm_storage_nod * 3) / 3 ))]})"
-    echo "$temp$(printf "%*s" $(($dwm_storage_len - ${#temp}))) $(printf "%02d" "$(df -h /dev/$(echo $temp) | tail -1 | awk '{print $5}' | sed 's/.$//' )")%"
+    temp="$( echo ${dwm_storage_drives[$(( $dwm_bar_loops % ($dwm_storage_nod * 3) / 3 ))]})"
+    echo "$temp$( printf "%*s" $(( $dwm_storage_len - ${#temp} )) ) $( printf "%02d" "$( df -h /dev/$( echo $temp ) | tail -1 | awk '{ print $5 }' | sed 's/.$//' )" )%"
 }
 
 # dwm_battery: a dwm_bar funcion to display current battery percentage and state
@@ -74,7 +74,7 @@ dwm_battery() {
     # Discharging: 69%
 
     # IMPORTANT NOTE: sometimes battery status might be 'UNKNOWN', and that is okay!
-    echo "$(cat /sys/class/power_supply/BAT0/status): $(cat /sys/class/power_supply/BAT0/capacity)%"
+    echo "$( cat /sys/class/power_supply/BAT0/status ): $( cat /sys/class/power_supply/BAT0/capacity )%"
 }
 
 # dwm_brightness: a dwm_bar function to display current brightness1
@@ -85,8 +85,8 @@ dwm_brightness() { # depends on brightnessctl
     # BL: 69%
 
     # IMPORTANT NOTE: requires brightnessctl, percentage will always be so that output does not differ in width (05% instead of 5%, MAX instead of 100%)
-    brightness=$( printf "%02d%%" "$(( $(brightnessctl get) * 100 / $(brightnessctl max) ))" )
-    if [ brightness = "100%" ]; then
+    brightness=$( printf "%02d%%" "$(( $( brightnessctl get ) * 100 / $( brightnessctl max ) ))" )
+    if [ $brightness = "100%" ]; then
         brightness="MAX"
     fi
     echo "BL: $brightness"
@@ -100,15 +100,15 @@ dwm_date() {
     # [4] 1970-01-01 00:00
 
     # IMPORTANT NOTE: i love iso-8601
-    echo "$(date "+[%u] %F %R")"
+    echo "$( date "+[%u] %F %R" )"
 }
 
 # update loop
 while true; do
     # set name to our new info and do it again 1 second(s) later
     # IMPORTANT NOTE, since dwm_track can return nothing we add the separation in its function, if you change order of things please modify dwm_track accordingly
-    xsetroot -name "$(dwm_track)$(dwm_memory) | $(dwm_storage) | $(dwm_battery) | $(dwm_brightness) | $(dwm_date)"
+    xsetroot -name "$( dwm_track )$( dwm_memory ) | $( dwm_storage ) | $( dwm_battery ) | $( dwm_brightness ) | $( dwm_date )"
     # increment our number of loops
-    dwm_bar_loops=$(($dwm_bar_loops + 1))
+    dwm_bar_loops=$(( $dwm_bar_loops + 1 ))
     sleep 1 # change this to be minimum update rate
 done
